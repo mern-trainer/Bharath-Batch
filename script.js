ee44349f6d197e4b84f5fdcd40c09efbea8dc57d
MERN TRAINER <futuralabs.mern@gmail.com>
2025-01-04 15:40:59 +0530

    map and filter

--- script.js
+++ script.js
@@ -27,3 +27,37 @@ sum(10, 20, display)
 
 // **********************************************************************
 
+// map, filter
+
+// array filter => returns a new array containing elements that pass a test
+
+const arr = [20, 56, 38, 29, 10, 64, 89, 45, 32, 96]
+const array = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"]
+
+const res = arr.filter(function (element) {
+    return element % 2 == 0
+})
+
+const res2 = array.filter(function (element) {
+    return element.length > 5
+})
+
+console.log(res2);
+
+// array map => returns a new array with the results of calling a function for every element in the original array
+// map => array manipulation
+
+const result = arr.map(function (element) {
+    if (element % 2 == 0) {
+        return element * 2   
+    }
+    return element
+})
+
+console.log(result);
+
+const res3 = array.map(function (element) {
+    return element.toUpperCase()
+})
+
+console.log(res3);
