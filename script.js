c29fdf61cfdca5c64046c987f2b12c5437d575c6
MERN TRAINER <futuralabs.mern@gmail.com>
2025-01-04 15:03:15 +0530

    callback function

--- script.js
+++ script.js
@@ -1,58 +1,29 @@
-// const age = 10
-// DOM => Document Object Model => Programming interface for HTML documents => content, structure, style
-// array => collection of elements
-// srting => collection of characters => "hello"
-// css => cascading style sheet => style
+// callback function => a function that is passed as an argument to another function
 
-const errors = document.getElementsByClassName("form-error")
+// **********************************************************************
 
-function handleResetErrors() {
-    for (let i = 0; i < errors.length; i++){
-        errors[i].innerText = ""
-    }
+function display() {
+    console.log("Display function called");
 }
 
-function handleSubmit() {
-
-    const username = document.getElementById("username").value
-    const password = document.getElementById("password").value
-
-    if (username == "" && password == "") {
-        errors[0].innerText = "Username is required"
-        errors[1].innerText = "Password is required"
-        return false
-    }
-
-    if (username == "") {
-        errors[0].innerText = "Username is required"
-        return false
-    } 
-
-    if (username.length < 4) {
-        errors[0].innerText = "Username must be atleast 4 characters"
-        return false
-    }
-
-    if (username.length > 14) {
-        errors[0].innerText = "Username must be atmost 14 characters"
-        return false
-    }
-
-    if (password == "") {
-        errors[1].innerText = "Password is required"
-        return false
-    }
-
-    if (password.length < 8) {
-        errors[1].innerText = "Password must be atleast 8 characters"
-        return false
-    }
-
-    if (password.length > 16) {
-        errors[1].innerText = "Password must be atmost 16 characters"
-        return false
-    }
-
-    alert("Form submitted successfully")
-    return false
+function myFunction(callbackFunc) {
+    callbackFunc()
 }
+
+myFunction(display)
+
+// **********************************************************************
+
+function display(value) {
+    console.log(value);
+}
+
+function sum(a, b, callbackFunc) {
+    const r = a + b
+    callbackFunc(r)
+}
+
+sum(10, 20, display)
+
+// **********************************************************************
+
