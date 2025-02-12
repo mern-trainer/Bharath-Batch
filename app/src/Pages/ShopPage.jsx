68d160017ae0109fa745c98f0ff0c0253cd35556
MERN-TRAINER <futuralabs.mern@gmail.com>
2025-02-12 15:12:17 +0530

    grid system

--- app/src/Pages/ShopPage.jsx
+++ app/src/Pages/ShopPage.jsx
@@ -1,13 +1,184 @@
 import { Fragment } from "react"
-import { Col, Container, Row } from "react-bootstrap"
+import { Button, Card, Col, Container, Row } from "react-bootstrap"
 
 const ShopPage = () => {
-    return <Container fluid>
-        <Row>
-            <Col xs="12" sm="6" md="4" lg="3" className="bg-success">10</Col>
-            <Col xs="12" sm="6" md="4" lg="3" className="bg-danger">10</Col>
-            <Col xs="12" sm="6" md="4" lg="3" className="bg-primary">10</Col>
-            <Col xs="12" sm="6" md="12" lg="3" className="bg-secondary">10</Col>
+    return <Container fluid className="pt-2">
+        <Row className="g-2">
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+
+
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+
+
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+
+            <Col xs="12" sm="6" md="4" xl="3">
+                <Card className="p-2">
+                    <Card.Img className="rounded mb-2" variant="top" src="https://t4.ftcdn.net/jpg/07/12/88/97/360_F_712889784_kM4G1JgbFsVp9IkQQn11qpijAI7nRi4I.jpg" />
+                    <Card.Body className="p-0">
+                        <Card.Title>Electronics Images</Card.Title>
+                        <Card.Text>Browse 9,618,524 Stock Photos, Vectors, and Video | Adobe Stock</Card.Text>
+                        <Button variant="secondary" className="w-100">Visit Url</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
         </Row>
     </Container>
 }
