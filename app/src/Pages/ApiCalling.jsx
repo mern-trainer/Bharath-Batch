372ef0852384829820f13166a8dec731cebe2141
MERN-TRAINER <futuralabs.mern@gmail.com>
2025-03-04 15:51:28 +0530

    user list: api call

--- app/src/Pages/ApiCalling.jsx
+++ app/src/Pages/ApiCalling.jsx
@@ -14,13 +14,15 @@ const ApiCalling = () => {
 
     const [users, setUsers] = useState([])
     const [page, setPage] = useState(1)
+    const [totalUsers, setTotalUsers] = useState(0)
 
     const getUsers = async () => {
         try {
-            const limit = 10
+            const limit = 30
             const skip = (page - 1) * limit
             const { data } = await api.get(`/users?limit=${limit}&skip=${skip}`)
             setUsers(users => ([...users, ...data.users]));
+            setTotalUsers(data.total)
         } catch (err) {
             return toast.error("Error occured while fetching users.")
         }
@@ -33,20 +35,25 @@ const ApiCalling = () => {
     return <div>
         <h2 className="text-center text-uppercase mt-2 mb-4">List Of Users</h2>
         <div className="container-fluid">
-            <div className="row p-2">
+            <div className="text-center mb-2">Loaded: {users.length}/{totalUsers} users</div>
+            <div className="row g-2">
                 {
                     users.map(user => {
-                        return <div key={user.id} className="col-12 p-3 col-sm-6 col-md-4 col-xl-3  text-start">
-                            <div className="d-flex align-items-center justify-content-start gap-2"><img src={user.image} alt={user.firstName} className="rounded-circle me-2" style={{width: "30px", height: "30px"}}/><h5 className="text-truncate">{user.firstName} {user.lastName}</h5></div>
-                            <div className="text-truncate">Email: {user.email}</div>
-                            <div className="text-truncate">Mobile: {user.phone}</div>
-                            <div className="text-truncate">DOB: {user.birthDate}</div>
-                            <div className="text-truncate">Gender: {user.gender}</div>
+                        return <div key={user.id} className="col-12 col-sm-6 col-md-4 col-xl-3 text-start">
+                            <div className="bg-light p-3 rounded">
+                                <div className="d-flex align-items-center justify-content-start gap-2"><img src={user.image} alt={user.firstName} className="rounded-circle me-2" style={{width: "30px", height: "30px"}}/><h5 className="text-truncate">{user.firstName} {user.lastName}</h5></div>
+                                <div className="text-truncate mt-1 w-100">Email: {user.email}</div>
+                                <div className="text-truncate mt-1 w-100">Mobile: {user.phone}</div>
+                                <div className="text-truncate mt-1 w-100">DOB: {user.birthDate}</div>
+                                <div className="text-truncate mt-1 w-100">Gender: {user.gender}</div>
+                                <div className="text-truncate mt-1 w-100">Age: {user.age}</div>
+                                <div className="text-truncate mt-1 w-100">Blood Group: {user.bloodGroup}</div>
+                            </div>
                         </div>
                     })
                 }
             </div>
-            <div className="text-center my-3" style={{cursor: "pointer"}} onClick={() => setPage(page => page + 1)}>More <FaArrowRotateRight /></div>
+            {users.length != totalUsers && <div className="text-center my-3" style={{ cursor: "pointer" }} onClick={() => setPage(page => page + 1)}>More <FaArrowRotateRight /></div>}
         </div>
     </div>
 }
